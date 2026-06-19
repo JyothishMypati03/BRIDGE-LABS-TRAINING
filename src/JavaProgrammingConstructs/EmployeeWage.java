edbf0a1f4df666029fc51e15d4c5867def62943c
Jyothish <jyothish1315@gmail.com>
2026-06-19 15:32:51 +0530

    [Jyothish Mypati] -- EmployeeWage

---
+++ src/JavaProgrammingConstructs/EmployeeWage.java
@@ -0,0 +1,21 @@
+package JavaProgrammingConstructs;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+
+
+        int EmpRatePerHour = 20;
+        int EMPHOURS = 8;
+
+            int empWage = EmpRatePerHour  * EMPHOURS;
+
+
+            System.out.println("Daily Employee Wage = " + empWage);
+
+
+    }
+
+
+
+}
