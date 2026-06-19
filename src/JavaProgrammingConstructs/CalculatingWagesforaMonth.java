c5253dabd2e66fd360ad27b677931f9dc52b84af
Jyothish <jyothish1315@gmail.com>
2026-06-19 15:59:08 +0530

    [Jyothish Mypati] -- CalculatingWagesforaMonth

---
+++ src/JavaProgrammingConstructs/CalculatingWagesforaMonth.java
@@ -0,0 +1,16 @@
+package JavaProgrammingConstructs;
+
+public class CalculatingWagesforaMonth {
+
+    public static void main(String[] args) {
+
+        int EmpRatePerHour = 20;
+        int WorkingDays = 20;
+        int FULL_DAY_HOURS = 8;
+
+        int monthlyWage = EmpRatePerHour * FULL_DAY_HOURS * WorkingDays;
+
+        System.out.println("Monthly Wage = " + monthlyWage);
+    }
+
+}
