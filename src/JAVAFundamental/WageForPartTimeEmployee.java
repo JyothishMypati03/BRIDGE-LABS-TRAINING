dec59a24843f60494875b20d5da1e1db757654ed
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:09:24 +0530

    [Jyothish Mypati -- WageForPartTimeEmployee]

---
+++ src/JAVAFundamental/WageForPartTimeEmployee.java
@@ -0,0 +1,17 @@
+package JAVAFundamental;
+
+public class WageForPartTimeEmployee {
+
+    public static  void main (String[] args){
+
+        int wageForHour = 10;
+        int Workinghours =  8 ;
+
+        int wage = wageForHour * Workinghours;
+
+        System.out.println("The wage for Part time Employee :"+ wage);
+
+    }
+
+
+}
