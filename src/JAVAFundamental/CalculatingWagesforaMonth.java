e494cfa2c232c582d1d6fc7563d8501df04ef82f
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:00:55 +0530

    [Jyothish Mypati -- CalculatingWagesforaMonth]

---
+++ src/JAVAFundamental/CalculatingWagesforaMonth.java
@@ -0,0 +1,13 @@
+package JAVAFundamental;
+
+public class CalculatingWagesforaMonth {
+
+    public  static  void  main (String[] args){
+
+        int wagePerDay = 500;
+        int workingDays = 20;
+
+        System.out.println("Wage for 20 days :" + wagePerDay * workingDays);
+
+    }
+}
