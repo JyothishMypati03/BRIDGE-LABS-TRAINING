1a954fb274d3bd7fd82cb6ba14e9ea98cc1b2112
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:27:31 +0530

    [Jyothish Mypati -- SumOfNumbers]

---
+++ src/AccessmodifiersOperators/SumOfNumbers.java
@@ -0,0 +1,23 @@
+package AccessmodifiersOperators;
+import  java.util.*;
+
+public class SumOfNumbers {
+
+    public  static  void  main(String[] args){
+
+        int n = 5;
+
+        int res = 0 ;
+        for(int i=1; i<=5; i++){
+
+            res = res + i;
+
+
+        }
+
+        System.out.println(res);
+
+
+    }
+
+}
