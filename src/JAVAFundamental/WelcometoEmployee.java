097339c2e80e1bfeb781006a9ad5c52c87a53306
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:11:04 +0530

    [Jyothish Mypati -- WelcometoEmployee]

---
+++ src/JAVAFundamental/WelcometoEmployee.java
@@ -0,0 +1,13 @@
+package JAVAFundamental;
+
+public class WelcometoEmployee {
+
+    public static void  main (String[] args){
+
+        System.out.println("Welcome to Employee\n" +
+                "Wage Computation\n" +
+                "Program");
+
+    }
+
+}
