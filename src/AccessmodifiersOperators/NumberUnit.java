ca86fd71e191820345d35ed5d28df5b3314945c5
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:25:07 +0530

    [Jyothish Mypati -- NumberUnit]

---
+++ src/AccessmodifiersOperators/NumberUnit.java
@@ -0,0 +1,26 @@
+package AccessmodifiersOperators;
+import  java.util.*;
+
+public class NumberUnit {
+
+    public  static  void  main(String[] args){
+
+       Scanner scanner =   new Scanner(System.in);
+
+       int input = scanner.nextInt();
+
+       switch (input){
+
+           case  10 : System.out.println("TEN"); break;
+           case  1 : System.out.println("ONE"); break;
+           case  100 : System.out.println("HUNDREAD"); break;
+
+
+       }
+
+
+
+    }
+
+
+}
