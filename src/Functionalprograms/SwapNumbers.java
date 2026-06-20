ed54f5481eda236519006be623f31969f5e8cef3
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:21:08 +0530

    [Jyothish Mypati -- SwapNumbers]

---
+++ src/Functionalprograms/SwapNumbers.java
@@ -0,0 +1,20 @@
+package Functionalprograms;
+
+public class SwapNumbers {
+
+    public  static void main (String[] args){
+
+        int a = 15 ;
+        int b = 20 ;
+
+        a = a + b;
+        b = a - b ;
+        a = a - b;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+
+}
