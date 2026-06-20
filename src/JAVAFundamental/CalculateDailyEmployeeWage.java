1b15e8bbc4a3ec964bd8ff85065b4b58c3788563
Jyothish <jyothish1315@gmail.com>
2026-06-20 15:59:53 +0530

    Jyothish Mypati -- CalculateDailyEmployeeWage]

---
+++ src/JAVAFundamental/CalculateDailyEmployeeWage.java
@@ -0,0 +1,21 @@
+package JAVAFundamental;
+import java.util.*;
+
+public class CalculateDailyEmployeeWage {
+
+    public static  void main (String[] args){
+
+        int wagePerhour = 20;
+        int workingHours = 8;
+
+        int wage = wagePerhour * workingHours;
+
+        System.out.println("The wage for daily employee  :" +wage);
+
+
+
+    }
+
+
+
+}
