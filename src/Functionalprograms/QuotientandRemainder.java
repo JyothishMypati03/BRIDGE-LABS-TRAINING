08dd7cef51f78ff97d19fd385361eff4e8038adf
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:20:16 +0530

    [Jyothish Mypati -- QuotientandRemainder]

---
+++ src/Functionalprograms/QuotientandRemainder.java
@@ -0,0 +1,18 @@
+package Functionalprograms;
+import java.util.*;
+
+public class QuotientandRemainder {
+
+    public static void  main (String[] args){
+
+       Scanner scanner  = new Scanner(System.in);
+
+       int N = 50 ;
+
+       System.out.println(N/2);
+       System.out.println(N%2);
+
+
+    }
+
+}
