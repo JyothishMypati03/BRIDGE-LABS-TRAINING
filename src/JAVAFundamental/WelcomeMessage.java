73edb604a4fbb2c1bc73aa9eee9cf0b3ec0a4bb0
Jyothish <jyothish1315@gmail.com>
2026-06-20 16:10:18 +0530

    [Jyothish Mypati -- WelcomeMessage]

---
+++ src/JAVAFundamental/WelcomeMessage.java
@@ -0,0 +1,13 @@
+package JAVAFundamental;
+import java.util.*;
+
+public class WelcomeMessage {
+
+    public  static  void main (String[] args){
+
+
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+
+}
