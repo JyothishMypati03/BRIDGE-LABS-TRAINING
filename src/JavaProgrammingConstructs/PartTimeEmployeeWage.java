b5bfd7191c0b918e2098430bde1e0efe87f655a1
Jyothish <jyothish1315@gmail.com>
2026-06-20 15:56:30 +0530

    [Jyothish Mypati -- PartTimeEmployeeWage]

---
+++ src/JavaProgrammingConstructs/PartTimeEmployeeWage.java
@@ -0,0 +1,20 @@
+package JavaProgrammingConstructs;
+
+public class PartTimeEmployeeWage {
+
+    public static void main(String[] args) {
+
+
+        int EmpRatePerHour = 20;
+        int EMPHOURS = 8;
+
+        int empWage = EmpRatePerHour  * EMPHOURS;
+
+
+        System.out.println("Daily Employee Wage = " + empWage);
+
+
+    }
+
+
+}
