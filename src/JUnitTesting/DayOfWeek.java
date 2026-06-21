0bcd8b305dd752b97fbea9ba50f5e13558df7b76
JyothishMypati03 <jyothish1315@gmail.com>
2026-06-21 20:33:50 +0530

    [Jyothish Mypati -- DayOfWeek]

---
+++ src/JUnitTesting/DayOfWeek.java
@@ -0,0 +1,42 @@
+package JUnitTesting;
+
+public class DayOfWeek {
+
+    public static int dayOfWeek(int m, int d, int y) {
+
+        int y0 = y - (14 - m) / 12;
+
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+
+        return d0;
+    }
+
+    public static void main(String[] args) {
+
+        int m = Integer.parseInt(args[0]);
+        int d = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        int result = dayOfWeek(m, d, y);
+
+        System.out.println("Day Number = " + result);
+
+        String[] days = {
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+        };
+
+        System.out.println("Day = " + days[result]);
+    }
+
+
+}
