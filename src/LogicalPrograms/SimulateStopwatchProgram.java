e2263eb2b25c93dd7081b6d8c1020cf42bab3101
JyothishMypati03 <jyothish1315@gmail.com>
2026-06-21 16:51:05 +0530

    [Jyothish Mypati -- SimulateStopwatchProgram]

---
+++ src/LogicalPrograms/SimulateStopwatchProgram.java
@@ -0,0 +1,27 @@
+package LogicalPrograms;
+import  java.util.*;
+
+public class SimulateStopwatchProgram {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Press Enter to Start Stopwatch...");
+        sc.nextLine();
+
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("Press Enter to Stop Stopwatch...");
+        sc.nextLine();
+
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Elapsed Time = " + elapsedTime + " milliseconds");
+        System.out.println("Elapsed Time = " + (elapsedTime / 1000.0) + " seconds");
+
+
+    }
+
+}
