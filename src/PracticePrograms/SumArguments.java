20b24492bcfd17fbbe9f0ddc18c929be459b3347
MYPATI JYOTHISH <jyothish1315@gmail.com>
2026-06-21 15:56:58 +0530

    Jyothish Mypati -- SumArguments

--- src/PracticePrograms/SumArguments.java
+++ src/PracticePrograms/SumArguments.java
@@ -1,4 +1,22 @@
 package PracticePrograms;
 
 public class SumArguments {
+
+ public static void main(String[] args) {
+        int sum = 0;
+        int invalidCount = 0;
+
+        for (String arg : args) {
+            try {
+                sum += Integer.parseInt(arg);
+            } catch (NumberFormatException e) {
+                invalidCount++;
+            }
+        }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Invalid Integers = " + invalidCount);
+    }
+
+  
 }
