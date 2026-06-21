98277a6e05e81655ec935bac5d73607cc4957f1b
MYPATI JYOTHISH <jyothish1315@gmail.com>
2026-06-21 15:54:58 +0530

    Jyothish Mypati -- StaticDemo

--- src/PracticePrograms/StaticDemo.java
+++ src/PracticePrograms/StaticDemo.java
@@ -1,4 +1,24 @@
 package PracticePrograms;
 
 public class StaticDemo {
+
+   static int count;
+
+
+    static {
+        count = 100;
+        System.out.println("Static block executed.");
+    }
+
+
+    static void display() {
+        System.out.println("Static Method Called.");
+        System.out.println("Count = " + count);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main Method Executed.");
+        display();
+    }  
+  
 }
